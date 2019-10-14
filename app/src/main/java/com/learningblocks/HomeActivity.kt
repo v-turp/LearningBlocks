65315fdfd742067ae4e775233527e3b79f3d86cc
victory <vturp@yahoo.com>
2019-10-14 14:11:39 -0700

    Initial commit. Includes all initial work. Work in progress. Committing to save current status.

---
+++ app/src/main/java/com/learningblocks/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.learningblocks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        setSupportActionBar(findViewById(R.id.wallet_toolbar))
+    }
+}
